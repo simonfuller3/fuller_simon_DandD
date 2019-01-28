cbb0654cd297141aa2af25120a8c10f3c707d1eb
Fuller <s_fuller3@tss-hr420-dm07.fc.ca>
2019-01-28 16:32:54 -0500

    updated js files

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -7,17 +7,17 @@
 
 	//function changeHeaderText() {
 	//	document.querySelector("h1").textContent = " Hey there from yo mamas house"
-	}
+	
 	
 	//function changeParagraphText() {
 	//	document.querySelector("p").textContent = "If you are reading this, run for your life!"
-	}
+	
 
 	//theButton.addEventListener("click", changeHeaderText);
 
-})();
 
-(() => {
+
+
 	// variables always come first//
 	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
@@ -26,21 +26,33 @@
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
 		//function always go in the middle//
-		function createPuzzlePieces() {
+		function createPuzzlePieces(pictureIndex) {
 			//generate puzzle pieces for the left hand side//
-			debugger;
+			pieces.forEach((piece, index) => {
+				let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+			
+					piecesBoard.innerHTML += newPuzzlePiece;
+			});
+
+			puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`
+
 		}
+
+
 
 		function resetPuzzlePieces() {
 			// swap out all of the images when clocking ona bottom button//
-			debugger;
+			// emptu the thumbnail container//
+			piecesBoard.innerHTML = "";
+			createPuzzlePieces(this.dataset.puzzleref);
+		
 
 		}
 		//event handling down here / at the bottom//
 		
-		puzzleSelectors.forEach(puzzle =>.addEventListener("click", resetPuzzlePieces));
+		puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
 
 
-		createPuzzlePieces();
+		createPuzzlePieces(0);
 
 })();

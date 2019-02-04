bc5068e403c5cdae29a046f645ab0a005a625644
Fuller <s_fuller3@tss-hr420-dm07.fc.ca>
2019-02-04 16:39:47 -0500

    js css and html update with loops

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -25,19 +25,50 @@
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+	let dropZones = document.querySelectorAll('.drop-zone');
+
 		//function always go in the middle//
 		function createPuzzlePieces(pictureIndex) {
 			//generate puzzle pieces for the left hand side//
 			pieces.forEach((piece, index) => {
-				let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+				let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
 			
 					piecesBoard.innerHTML += newPuzzlePiece;
 			});
 
 			puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`
 
+		
+			initDrag();
 		}
 
+
+		function initDrag() {
+			piecesBoard.querySelectorAll('img').forEach(img => {
+				img.addEventListener("dragstart", function(e) {
+					//e.preventDefault();//
+					console.log('draggin...')
+				
+					e.dataTransfer.setData("text/plain", this.id);
+				});
+			});
+		}
+
+		// handle dragover and drop//
+		dropZones.forEach(zone => {
+			zone.addEventListener("dragover", function(e) {
+				e.preventDefault();
+				console.log("you dragged over me!");
+			});
+
+			zone.addEventListener("drop", function(e) {
+				e.preventDefault();
+				console.log("you dropped it on me");
+			
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+			});
+		});
 
 
 		function resetPuzzlePieces() {
